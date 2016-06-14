620e2d8675fb974404989e5cf71f247ed4917bcd
Brady Bryan <jbraden.bryan@gmail.com>
2016-06-13 23:37:36 -0500

    adds functionality to rotate the squares

--- public/src/color-squares.js
+++ public/src/color-squares.js
@@ -1,23 +1,46 @@
 var Square = React.createClass({
   render: function() {
-    var className = 'square ' + this.colorMap[this.props.squareNum];
+    var className = 'square ' + this.props.color;
     return (
       <div className={className}></div>
     );
-  },
-  colorMap: ['red', 'blue', 'green', 'yellow']
+  }
 });
 
 var SquareMaker = React.createClass({
   render: function() {
     var squares = [];
     for (var i = 0; i < this.props.squareCount; i++) {
-      squares.push(<Square squareNum={i} key={i}/>);
+      var color = this.state.colorMap[i];
+      squares.push(<Square squareNum={i} key={i} color={color}/>);
     }
     return (
-      <div className="squareContainer">{squares}</div>
+      <div className="squareContainer">
+        {squares}
+        <button onClick={this.onLeftClick}>Rotate Left</button>
+        <button onClick={this.onRightClick}>Rotate Right</button>
+      </div>
     );
-  }
+  },
+  getInitialState: function() {
+    return {
+      colorMap: ['red', 'blue', 'green', 'yellow']
+    };
+  },
+  onLeftClick: function(e) {
+    var colorMap = this.state.colorMap;
+    colorMap.push(colorMap.shift());
+    this.setState({
+      colorMap: colorMap
+    });
+  },
+  onRightClick: function(e) {
+    var colorMap = this.state.colorMap;
+    colorMap.unshift(colorMap.pop());
+    this.setState({
+      colorMap: colorMap
+    });
+  },  
 });
 
 ReactDOM.render(
